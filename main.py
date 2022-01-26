f86b72d1ae9986b71cba4a7082e642a428ab004e
Skyrah1 <zhunchenyek1@gmail.com>
2022-01-26 20:21:47 +0000

    Move functionality of adding and updating vehicles to a new VehicleShepherd class

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os, sys
 import dotenv
 import traci
 import sumolib
-from vehicle import Vehicle
+from vehicle_shepherd import VehicleShepherd
 
 if "SUMO_HOME" in os.environ:
     tools = os.path.join(os.environ["SUMO_HOME"], "tools")
@@ -24,27 +24,15 @@ def run_simulation(vehicleIds_to_routes:dict):
 
     traci.start(sumoCmd)
 
-    vehicles = {}
-    for vId in vehicleIds_to_routes:
-        vehicle:Vehicle = Vehicle(vId)
-        vehicle.add_to_route(vehicleIds_to_routes[vId])
-        vehicles[vId] = vehicle
+    vehicle_shepherd = VehicleShepherd()
+    vehicle_shepherd.add_vehicles(vehicleIds_to_routes)
     
     step = 0
-    while step < 100 and len(vehicles) > 0:
+    while step < 100 and len(vehicle_shepherd.vehicles) > 0:
         traci.simulationStep()
         vIds_to_be_removed = []
 
-        # Update all active vehicles
-        for vId in vehicles:
-            try:
-                vehicles[vId].update()
-            except traci.exceptions.TraCIException as e:
-                print(e)
-                vIds_to_be_removed.append(vId)
-        # Stop tracking any vehicles that no longer exist
-        for vId in vIds_to_be_removed:
-            vehicles.pop(vId)
+        vehicle_shepherd.update_vehicles()
         # Print collisions that are currently happening
         print(traci.simulation.getCollisions())
         
