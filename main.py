593b44e41b72d9c385251db8aab243a6cffad84b
Skyrah1 <zhunchenyek1@gmail.com>
2022-01-26 16:27:19 +0000

    Improve how we keep track of vehicles

--- main.py
+++ main.py
@@ -23,23 +23,35 @@ def run_simulation(vehicleIds_to_routes:dict):
     sumoCmd = [sumoBinary, "-c", ENV["configPath"], "--collision.check-junctions", "--collision.action", "warn"]
 
     traci.start(sumoCmd)
+
+    vehicles = {}
     for vId in vehicleIds_to_routes:
         vehicle:Vehicle = Vehicle(vId)
         vehicle.add_to_route(vehicleIds_to_routes[vId])
+        vehicles[vId] = vehicle
+    
     step = 0
-    while step < 100:
+    while step < 100 and len(vehicles) > 0:
         traci.simulationStep()
-        
-        for vId in vehicleIds_to_routes:
+        vIds_to_be_removed = []
+
+        # Print speed and positions of all vehicles
+        for vId in vehicles:
             try:
                 message:str = "Speed of " + vId + ": " + str(traci.vehicle.getSpeed(vId))
                 message += "\nPosition of " + vId + ": " + str(traci.vehicle.getPosition(vId))
                 print(message)
             except traci.exceptions.TraCIException as e:
                 print(e)
+                vIds_to_be_removed.append(vId)
+        # Stop tracking any vehicles that no longer exist
+        for vId in vIds_to_be_removed:
+            vehicles.pop(vId)
+        # Print collisions that are currently happening
         print(traci.simulation.getCollisions())
         
         step += 1
+    
     traci.close()
 
 
