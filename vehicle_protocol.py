0c605942cbd65135f5a83185897d8b840a690e76
Skyrah1 <zhunchenyek1@gmail.com>
2022-01-30 14:44:45 +0000

    A comment

--- vehicle_protocol.py
+++ vehicle_protocol.py
@@ -2,6 +2,9 @@ from vehicle_state import VehicleState
 
 class VehicleProtocol:
     
+    # Default conflict resolution protocol.
+    # If any conflicting vehicles are detected that are driving, the vehicle just stops
+    # Otherwise, keep driving
     def decide_state(self, vehicle, conflicting_vehicles):
         for other_vehicle in conflicting_vehicles:
             if other_vehicle.currentState == VehicleState.DRIVING:
