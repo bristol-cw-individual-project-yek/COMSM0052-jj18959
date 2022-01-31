625aa2bfb01400ac90420abd39edb5073e59573f
Skyrah1 <zhunchenyek1@gmail.com>
2022-01-31 20:15:14 +0000

    Update to test CI picks up failing test

--- vehicle/test/test_grid.py
+++ vehicle/test/test_grid.py
@@ -10,6 +10,6 @@ class TestGrid(unittest.TestCase):
         self.assertEqual(grid.position_to_grid_square((0, 5)), (0, 0))
         self.assertEqual(grid.position_to_grid_square((10, 15)), (1, 1))
         self.assertEqual(grid.position_to_grid_square((-1, -10)), (-1, -1))
-        self.assertEqual(grid.position_to_grid_square((-1, -10.000000000001)), (-1, -2))
+        self.assertEqual(grid.position_to_grid_square((-1, -10)), (-1, -2))
         self.assertEqual(grid.position_to_grid_square((-22, 100)), (-3, 10))
         self.assertEqual(grid.position_to_grid_square((-11.3, 152.5)), (-2, 15))
