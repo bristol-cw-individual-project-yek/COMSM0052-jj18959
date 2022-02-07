86c6e2367be60121f1e54ca3f42ec02e7a45aafe
Skyrah1 <zhunchenyek1@gmail.com>
2022-02-07 13:57:13 +0000

    Tests

---
+++ test/test_main.py
@@ -0,0 +1,10 @@
+import unittest
+import main
+
+class TestMain(unittest.TestCase):
+
+    def test_run_simulation(self):
+        vehicleIdsToRoutes = {}
+        for i in range(100):
+            vehicleIdsToRoutes["testVehicle"] = "r" + str(i)
+            main.run_simulation(vehicleIdsToRoutes)
