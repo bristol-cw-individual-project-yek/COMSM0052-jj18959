35eae2a32d05d5d80661b7d38da77d1f648312fe
Skyrah1 <zhunchenyek1@gmail.com>
2022-02-07 14:16:30 +0000

    Update test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -5,6 +5,6 @@ class TestMain(unittest.TestCase):
 
     def test_run_simulation(self):
         vehicleIdsToRoutes = {}
-        for i in range(100):
+        for i in range(10):
             vehicleIdsToRoutes["testVehicle"] = "r" + str(i)
             main.run_simulation(vehicleIdsToRoutes)
