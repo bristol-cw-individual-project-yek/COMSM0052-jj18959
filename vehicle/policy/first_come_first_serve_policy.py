881c63e2b114f2c88211fc83c7328dfb92010ac4
Skyrah1 <zhunchenyek1@gmail.com>
2022-02-14 18:48:11 +0000

    Fixed issue with vehicles stopping in the middle of the junction, but now there are problems with cars not entering the junction at all

--- vehicle/policy/first_come_first_serve_policy.py
+++ vehicle/policy/first_come_first_serve_policy.py
@@ -2,14 +2,16 @@ from vehicle.policy.policy import Policy
 from vehicle.vehicle_state import VehicleState
 
 class FirstComeFirstServePolicy(Policy):
-    MIN_DISTANCE_FROM_JUNCTION = 5
+    MIN_DISTANCE_FROM_JUNCTION = 8
     MIN_DISTANCE_FROM_VEHICLE = 10
-    
+
     def decide_state(self, vehicle, conflicting_vehicles:list):
+        next_junction = vehicle.nextJunction
         distance_to_junction = vehicle.get_distance_to_junction()
         for other_vehicle in conflicting_vehicles:
-            if other_vehicle.get_distance_to_junction() <= distance_to_junction and \
-            (vehicle.get_distance_to_vehicle(other_vehicle) <= FirstComeFirstServePolicy.MIN_DISTANCE_FROM_VEHICLE or \
-            vehicle.get_distance_to_junction() <= FirstComeFirstServePolicy.MIN_DISTANCE_FROM_JUNCTION):
+            if other_vehicle.nextJunction.getID() == next_junction.getID() and \
+                other_vehicle.get_distance_to_junction() <= distance_to_junction and \
+                (vehicle.get_distance_to_vehicle(other_vehicle) <= FirstComeFirstServePolicy.MIN_DISTANCE_FROM_VEHICLE or \
+                vehicle.get_distance_to_junction() <= FirstComeFirstServePolicy.MIN_DISTANCE_FROM_JUNCTION):
                 return VehicleState.WAITING
         return VehicleState.DRIVING
