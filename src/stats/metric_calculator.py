2fa659e5427c50dbebd691b0b69fd43e17093b5a
Skyrah1 <zhunchenyek1@gmail.com>
2022-03-28 20:24:21 +0100

    Add additional metrics
    - Median, min and max waiting times
    - Stats related to wait times per junction

--- src/stats/metric_calculator.py
+++ src/stats/metric_calculator.py
@@ -4,19 +4,28 @@ import numpy as np
 class MetricCalculator:
 
     def calculate(vehicles:dict) -> dict:
-        waiting_times = []
+        total_waiting_times:list = []
+        wait_times_per_junction:list = []
         for vId in vehicles:
-            waiting_times.append(vehicles[vId].totalTimeSpentWaiting)
-        
-        mean = np.mean(waiting_times)
-        # Non-adjusted Fisher-Pearson coefficient of skewness
-        skew = stats.skew(waiting_times)
-        kurtosis = stats.kurtosis(waiting_times)
-        
+            total_waiting_times.append(vehicles[vId].totalTimeSpentWaiting)
+            wait_times_per_junction.extend(vehicles[vId].pastWaitTimesAtJunctions)
         results = {
-            "wait-time-mean"        : mean,
-            "wait-time-skew"        : skew,
-            "wait-time-kurtosis"    : kurtosis
+            "total-wait-time"           : {
+                "mean"      : np.mean(total_waiting_times),
+                "median"    : np.median(total_waiting_times),
+                "min"       : int(np.min(total_waiting_times)),
+                "max"       : int(np.max(total_waiting_times)),
+                "skew"      : stats.skew(total_waiting_times),      # Non-adjusted Fisher-Pearson coefficient of skewness
+                "kurtosis"  : stats.kurtosis(total_waiting_times),
+            },
+            "wait-times-per-junction"    : {
+                "mean"      : np.mean(wait_times_per_junction),
+                "median"    : np.median(wait_times_per_junction),
+                "min"       : int(np.min(wait_times_per_junction)),
+                "max"       : int(np.max(wait_times_per_junction)),
+                "skew"      : stats.skew(wait_times_per_junction),      # Non-adjusted Fisher-Pearson coefficient of skewness
+                "kurtosis"  : stats.kurtosis(wait_times_per_junction),
+            }
         }
 
         return results
