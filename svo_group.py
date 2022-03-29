76a377efc52e28870914104ecb56c679f04f8a99
Skyrah1 <zhunchenyek1@gmail.com>
2022-03-29 12:32:07 +0100

    Bug fixes to custom SVO group policy

--- svo_group.py
+++ svo_group.py
@@ -6,19 +6,19 @@ import copy
 class SVOGroupPolicy(Policy):
 
     def decide_state(self, vehicle:Vehicle, conflicting_vehicles: dict):
-        print("aaa")
-        if vehicle.get_distance_to_junction() <= SVOGroupPolicy.MIN_WAITING_DISTANCE_FROM_JUNCTION:
-            other_vehicles_at_junction = conflicting_vehicles["same_junction"]
+        other_vehicles_at_junction = conflicting_vehicles["same_junction"]
+        if vehicle.get_distance_to_junction() <= SVOGroupPolicy.MIN_WAITING_DISTANCE_FROM_JUNCTION and vehicle.currentState != VehicleState.CROSSING and len(other_vehicles_at_junction) > 0:
             svo_utility = vehicle.get_social_value_orientation_utility_group_average(other_vehicles_at_junction)
             for other_vehicle in other_vehicles_at_junction:
                 ov:Vehicle = other_vehicle
-                others = copy.copy(other_vehicles_at_junction)
-                others.add(vehicle)
+                others:list = copy.copy(other_vehicles_at_junction)
+                others.append(vehicle)
                 others.remove(ov)
-                other_svo_utility = ov.get_social_value_orientation_utility_group_average(others)
-                print(f"Comparing {svo_utility} with {other_svo_utility}")
-                if svo_utility < other_svo_utility:
-                    return VehicleState.WAITING
+                if len(others) > 0:
+                    other_svo_utility = ov.get_social_value_orientation_utility_group_average(others)
+                    print(f"{vehicle.vehicleId} is comparing {svo_utility} with {other_svo_utility}")
+                    if svo_utility < other_svo_utility:
+                        return VehicleState.WAITING
         
         for other_vehicle in conflicting_vehicles["same_lane"]:
             if self.is_conflicting_same_lane(vehicle, other_vehicle):
