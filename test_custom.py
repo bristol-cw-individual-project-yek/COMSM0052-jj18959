d4cf7af33767fdffc451eecebf06f3cac338171c
Skyrah1 <zhunchenyek1@gmail.com>
2022-03-29 11:50:07 +0100

    Update test_custom.py

--- test_custom.py
+++ test_custom.py
@@ -1,23 +1,24 @@
 from src.vehicle.policy.policy import Policy
+from src.vehicle.vehicle import Vehicle
 from src.vehicle.vehicle_state import VehicleState
 
 class CustomTestPolicy(Policy):
 
-    def decide_state(self, vehicle, conflicting_vehicles: dict):
+    def decide_state(self, vehicle:Vehicle, conflicting_vehicles: dict):
         return super().decide_state(vehicle, conflicting_vehicles)
     
 
-    def is_conflicting_same_junction(self, vehicle, other_vehicle) -> bool:
+    def is_conflicting_same_junction(self, vehicle:Vehicle, other_vehicle:Vehicle) -> bool:
         return super().is_conflicting_same_junction(vehicle, other_vehicle)
     
 
-    def is_conflicting_same_lane(self, vehicle, other_vehicle) -> bool:
+    def is_conflicting_same_lane(self, vehicle:Vehicle, other_vehicle:Vehicle) -> bool:
         return super().is_conflicting_same_lane(vehicle, other_vehicle)
     
 
-    def is_conflicting_visible(self, vehicle, other_vehicle) -> bool:
+    def is_conflicting_visible(self, vehicle:Vehicle, other_vehicle:Vehicle) -> bool:
         return super().is_conflicting_visible(vehicle, other_vehicle)
     
 
-    def decide_state_no_conflicts(self, vehicle) -> VehicleState:
+    def decide_state_no_conflicts(self, vehicle:Vehicle) -> VehicleState:
         return super().decide_state_no_conflicts(vehicle)
