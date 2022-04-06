a1be1250e34a834bc6143991e987eb3ab25cf7ae
Skyrah1 <zhunchenyek1@gmail.com>
2022-04-06 11:27:28 +0100

    Update fcfs_svo.py

--- fcfs_svo.py
+++ fcfs_svo.py
@@ -4,7 +4,11 @@ from src.vehicle.vehicle import Vehicle
 class FCFS_SVO_Policy(FCFS_CentralizedPolicy):
 
     def can_swap(self, vehicle: Vehicle, other_vehicle: Vehicle) -> bool:
-        if vehicle.get_social_value_orientation_utility_one_to_one(other_vehicle) > other_vehicle.get_social_value_orientation_utility_one_to_one(vehicle):
+        reserved_time = FCFS_SVO_Policy.reserved_times[vehicle]
+        other_reserved_time = FCFS_SVO_Policy.reserved_times[vehicle]
+        veh_svo_utility = vehicle.get_social_value_orientation_utility_one_to_one(other_vehicle, reserved_time, other_reserved_time)
+        other_svo_utility = other_vehicle.get_social_value_orientation_utility_one_to_one(vehicle, other_reserved_time, reserved_time)
+        if veh_svo_utility > other_svo_utility:
             return True
         else:
             return False
