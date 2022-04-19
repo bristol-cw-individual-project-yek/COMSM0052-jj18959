f8bfd922a54f0052d252f268bfa0d0533385a323
Skyrah1 <zhunchenyek1@gmail.com>
2022-04-19 17:55:39 +0100

    FCFS_SVO now uses correct formula

--- fcfs_svo.py
+++ fcfs_svo.py
@@ -6,9 +6,11 @@ class FCFS_SVO_Policy(FCFS_CentralizedPolicy):
     def can_swap(self, vehicle: Vehicle, other_vehicle: Vehicle) -> bool:
         reserved_time = FCFS_SVO_Policy.reserved_times[vehicle]
         other_reserved_time = FCFS_SVO_Policy.reserved_times[vehicle]
-        veh_svo_utility = vehicle.get_social_value_orientation_utility_one_to_one(other_vehicle, reserved_time, other_reserved_time)
-        other_svo_utility = other_vehicle.get_social_value_orientation_utility_one_to_one(vehicle, other_reserved_time, reserved_time)
-        if veh_svo_utility > other_svo_utility:
+        veh_svo_utility_old = vehicle.get_social_value_orientation_utility_one_to_one(other_vehicle, reserved_time, other_reserved_time)
+        veh_svo_utility_new = vehicle.get_social_value_orientation_utility_one_to_one(other_vehicle, other_reserved_time, reserved_time)
+        other_svo_utility_old = other_vehicle.get_social_value_orientation_utility_one_to_one(vehicle, other_reserved_time, reserved_time)
+        other_svo_utility_new = other_vehicle.get_social_value_orientation_utility_one_to_one(vehicle, reserved_time, other_reserved_time)
+        if veh_svo_utility_new > veh_svo_utility_old and other_svo_utility_new > other_svo_utility_old:
             return True
         else:
             return False
