109aa0b24ade0c840d0d01d64a36cf8beba80e72
Skyrah1 <zhunchenyek1@gmail.com>
2022-04-23 14:34:09 +0100

    Create ArbiterManager

--- src/arbiter/arbiter.py
+++ src/arbiter/arbiter.py
@@ -16,3 +16,24 @@ class ArbiterPolicy:
 class Arbiter:
     def __init__(self, policy:ArbiterPolicy):
         self.policy = policy
+
+
+class ArbiterManager:
+    def __init__(self):
+        self.__junction_to_arbiter:dict = {}
+    
+
+    def assign_arbiter_to_junction(self, junction_id:str, arbiter:Arbiter):
+        self.__junction_to_arbiter[junction_id] = arbiter
+    
+
+    def has_arbiter(self, junction_id:str):
+        return junction_id in self.__junction_to_arbiter
+    
+
+    def send_message_to_arbiter(self, junction_id:str, vehicle):
+        try:
+            arbiter:Arbiter = self.__junction_to_arbiter[junction_id]
+            return arbiter.policy.receive_message(vehicle)
+        except KeyError:
+            raise
