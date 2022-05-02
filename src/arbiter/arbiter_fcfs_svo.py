9225b994b1228cfcb8fd97fbd39e8d5f07a8aced
Skyrah1 <zhunchenyek1@gmail.com>
2022-05-02 23:33:35 +0100

    a

--- src/arbiter/arbiter_fcfs_svo.py
+++ src/arbiter/arbiter_fcfs_svo.py
@@ -3,20 +3,20 @@ from src.vehicle.vehicle import Vehicle
 
 class Arbiter_FCFS_SVO_Policy(ArbiterFCFSPolicy):
 
-    def can_swap(self, v_j: Vehicle, v_i: Vehicle) -> bool:
-        t_j = self.reserved_times[v_j]
-        t_i = self.get_arrival_time(t_j, v_i)
+    def can_swap(self, v_i: Vehicle, v_j: Vehicle) -> bool:
+        t_i = self.reserved_times[v_i]
+        t_j = self.get_arrival_time(t_i, v_j)
 
-        t_i_new = t_j
-        t_j_new = self.get_arrival_time(t_i_new, v_j)
+        t_j_new = t_i
+        t_i_new = self.get_arrival_time(t_j_new, v_i)
         
-        u_i_old = v_i.get_svo_utility_one_to_one(v_j, t_i, t_j)
-        u_i_new = v_i.get_svo_utility_one_to_one(v_j, t_i_new, t_j_new)
-
         u_j_old = v_j.get_svo_utility_one_to_one(v_i, t_j, t_i)
         u_j_new = v_j.get_svo_utility_one_to_one(v_i, t_j_new, t_i_new)
 
-        if (u_i_new > u_i_old and u_j_new > u_j_old):
+        u_i_old = v_i.get_svo_utility_one_to_one(v_j, t_i, t_j)
+        u_i_new = v_i.get_svo_utility_one_to_one(v_j, t_i_new, t_j_new)
+
+        if (u_j_new > u_j_old and u_i_new > u_i_old):
             return True
         else:
             return False
--- src/arbiter/arbiter_priority.py
+++ src/arbiter/arbiter_priority.py
@@ -3,8 +3,8 @@ from src.vehicle.vehicle import Vehicle
 
 class ArbiterPriorityPolicy(ArbiterFCFSPolicy):
 
-    def can_swap(self, v_j: Vehicle, v_i: Vehicle) -> bool:
-        if v_j.priority > v_i.priority:
+    def can_swap(self, v_i: Vehicle, v_j: Vehicle) -> bool:
+        if v_i.priority > v_j.priority:
             return True
         else:
             return False
